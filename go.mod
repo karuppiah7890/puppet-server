8048f5b1265bcc18d99aa392c3635dedb1cadc21
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-02-24 13:43:47 +0530

    introduce crash ping api to crash the ping api

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/karuppiah7890/puppet-server
+
+require (
+	github.com/AndreasBriese/bbloom v0.0.0-20180913140656-343706a395b7 // indirect
+	github.com/dgraph-io/badger v1.5.4
+	github.com/dgryski/go-farm v0.0.0-20190104051053-3adb47b1fb0f // indirect
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd // indirect
+	golang.org/x/sys v0.0.0-20190222171317-cd391775e71e // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/dgraph-io/badger"
 )
 
 // ResponseMessage represents the message sent by a server
@@ -26,11 +31,104 @@ func getJSONMessage(message string) ([]byte, error) {
 }
 
 func main() {
+	opts := badger.DefaultOptions
+	opts.Dir = "/Users/karuppiah/badger"
+	opts.ValueDir = "/Users/karuppiah/badger"
+	db, err := badger.Open(opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 
-		jsonResponse, err := getJSONMessage("pong")
+		err := db.View(func(txn *badger.Txn) error {
+			item, err := txn.Get([]byte("crash"))
+
+			if err == badger.ErrKeyNotFound {
+				jsonResponse, err := getJSONMessage("pong")
+
+				if err != nil {
+					return err
+				}
+
+				log.Printf("RequestMethod:%v RequestUrl:%v ResponseCode:%v", r.Method, r.URL.Path, http.StatusOK)
+				w.WriteHeader(http.StatusOK)
+				w.Write(jsonResponse)
+				return nil
+			}
+
+			if err != nil {
+				return err
+			}
+
+			crash, err := item.ValueCopy(nil)
+			if err != nil {
+				return err
+			}
+
+			if bytes.Compare(crash, []byte("yes")) == 0 {
+				jsonResponse, err := getJSONMessage("crashing")
+
+				if err != nil {
+					return err
+				}
+
+				log.Printf("RequestMethod:%v RequestUrl:%v ResponseCode:%v", r.Method, r.URL.Path, http.StatusServiceUnavailable)
+				w.WriteHeader(http.StatusServiceUnavailable)
+				w.Write(jsonResponse)
+				return nil
+			}
+
+			jsonResponse, err := getJSONMessage("pong")
+
+			if err != nil {
+				return err
+			}
+
+			log.Printf("RequestMethod:%v RequestUrl:%v ResponseCode:%v", r.Method, r.URL.Path, http.StatusOK)
+			w.WriteHeader(http.StatusOK)
+			w.Write(jsonResponse)
+			return nil
+		})
+
+		if err != nil {
+			log.Printf("error occurred: %v", err)
+			log.Printf("RequestMethod:%v RequestUrl:%v ResponseCode:%v", r.Method, r.URL.Path, http.StatusInternalServerError)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	})
+
+	mux.HandleFunc("/crashping", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		ttlTime := r.URL.Query().Get("time")
+		crash := r.URL.Query().Get("crash")
+
+		ttl, err := time.ParseDuration(ttlTime)
+
+		if err != nil {
+			log.Printf("error occurred while parsing time: %v", err)
+			log.Printf("RequestMethod:%v RequestUrl:%v ResponseCode:%v", r.Method, r.URL.Path, http.StatusBadRequest)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err = db.Update(func(txn *badger.Txn) error {
+			err := txn.SetWithTTL([]byte("crash"), []byte(crash), ttl)
+			return err
+		})
+
+		if err != nil {
+			log.Printf("error occurred while updating 'crash' key in db: %v", err)
+			log.Printf("RequestMethod:%v RequestUrl:%v ResponseCode:%v", r.Method, r.URL.Path, http.StatusInternalServerError)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		jsonResponse, err := getJSONMessage(fmt.Sprintf("crash set to %v with ttl %v !", crash, ttlTime))
 
 		if err != nil {
 			log.Printf("RequestMethod:%v RequestUrl:%v ResponseCode:%v", r.Method, r.URL.Path, http.StatusInternalServerError)
@@ -39,11 +137,12 @@ func main() {
 		}
 
 		log.Printf("RequestMethod:%v RequestUrl:%v ResponseCode:%v", r.Method, r.URL.Path, http.StatusOK)
+		w.WriteHeader(http.StatusOK)
 		w.Write(jsonResponse)
 	})
 
 	log.Printf("server is starting ...")
-	err := http.ListenAndServe(":8080", mux)
+	err = http.ListenAndServe(":8080", mux)
 
 	if err != nil {
 		log.Fatalf("failed to start the server : %v", err)
