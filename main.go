998fe8274a399461be4af3b6a22c66b88f72218c
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-02-24 12:31:23 +0530

    refactor formation of json message

--- main.go
+++ main.go
@@ -11,18 +11,28 @@ type ResponseMessage struct {
 	Message string `json:"message"`
 }
 
+func getJSONMessage(message string) ([]byte, error) {
+	responseMessage := ResponseMessage{
+		Message: message,
+	}
+	jsonResponse, err := json.Marshal(responseMessage)
+
+	if err != nil {
+		log.Printf("error occurred while json marshalling pong response: %v", err)
+		return nil, err
+	}
+
+	return jsonResponse, nil
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		message := ResponseMessage{
-			Message: "pong",
-		}
 
-		jsonResponse, err := json.Marshal(message)
+		jsonResponse, err := getJSONMessage("pong")
 
 		if err != nil {
-			log.Printf("error occurred while json marshalling pong response: %v", err)
 			log.Printf("RequestMethod:%v RequestUrl:%v ResponseCode:%v", r.Method, r.URL.Path, http.StatusInternalServerError)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
